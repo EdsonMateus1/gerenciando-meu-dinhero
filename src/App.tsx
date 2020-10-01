659fc50848e8d6e777c6c85ff169a3dc2a7258a8
EdsonMateus1 <tioed2015@gmailcom>
2020-10-01 13:23:06 -0300

    configurando cores e temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+
 import GlobalStyle from "./styles/GlobalStyle";
 import Layout from "./components/Layout";
 
+import dark from "./styles/themes/dark";
+import light from "./styles/themes/light";
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyle />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+export default {
+    title : "light",
+
+    colors:{
+        primary: "#dcdcdc",
+        secondary: "#ffff",
+        tertiary: "#f5f5f5",
+
+        white: "#000",
+        black: "#fff",
+        gray: "#bfbfbf",
+
+        success: "#03bb85",
+        info: "#3b5998",
+        warning: "#ff6961",
+    }
+}
